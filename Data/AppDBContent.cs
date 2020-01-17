0ffd169656ce5e71aea79d4f4371b20d626332fb
Aleksey Gunchin <a.s.gunchin@gmail.com>
2020-01-17 12:05:32 +0400

    Add database

--- Data/AppDBContent.cs
+++ Data/AppDBContent.cs
@@ -1,10 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using amti_bookShop.Data.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace amti_bookShop.Data {
-    public class AppDBContent : DbContext {
+  public class AppDBContent : DbContext {
+    public AppDBContent(DbContextOptions<AppDBContent> options) : base(options) {
+
     }
+    public DbSet<Book> Book { get; set; }
+    public DbSet<Category> Category { get; set; }
+  }
 }
--- Startup.cs
+++ Startup.cs
@@ -2,17 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using amti_bookShop.Data;
 using amti_bookShop.Data.Interfaces;
 using amti_bookShop.Data.Mocks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace amti_bookShop {
 	public class Startup {
+		private IConfigurationRoot _confSting;
+
+		[Obsolete]
+		public Startup(IHostingEnvironment hostEnv) {
+			_confSting = new ConfigurationBuilder().SetBasePath(hostEnv.ContentRootPath).AddJsonFile("dbsettings.json").Build();
+		}
 		public void ConfigureServices(IServiceCollection services) {
+			services.AddDbContext<AppDBContent>(options => options.UseSqlServer(_confSting.GetConnectionString("DefaultConnection")));
 			services.AddTransient<IAllBooks, MockBooks>();
 			services.AddTransient<IBooksCategory, MockCategory>();
 			services.AddMvc();
